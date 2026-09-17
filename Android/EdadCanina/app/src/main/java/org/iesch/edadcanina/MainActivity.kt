a1bc109044c4456f0785953d155f2f852f83c18a
arodriguezgim <arodriguezg@iesch.org>
2026-09-17 18:18:48 +0200

    App terminada

--- Android/EdadCanina/app/src/main/java/org/iesch/edadcanina/MainActivity.kt
+++ Android/EdadCanina/app/src/main/java/org/iesch/edadcanina/MainActivity.kt
@@ -1,6 +1,10 @@
 package org.iesch.edadcanina
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,7 +21,32 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         // Aquí la pantalla está creada.
-
         // 1 - Tomamos el control de todos los elementos de la parte de la UI
+        val resultText = findViewById<TextView>(R.id.texto_respuesta)
+        val calculateButton = findViewById<Button>(R.id.boton_calcular)
+        val ageEdit = findViewById<EditText>(R.id.edit_edad)
+
+        // 2 - Los botones tienen la propiedad setOnClickListener al pulsarlo
+        calculateButton.setOnClickListener {
+            // Aquí metemos el código de lo que queremos hacer cuando pulsemos el botón de Calcular
+            val edadString = ageEdit.text.toString()
+
+            if ( edadString.isEmpty() ){
+                //3 - Mostramos un mensaje de tipo Toast
+                Toast.makeText( this, R.string.texto_toast, Toast.LENGTH_LONG).show()
+
+            } else {
+                // Necesitamos pasar el valo a entero
+                val edadInt = edadString.toInt()
+                val dogAge = edadInt * 7
+
+                //println( dogAge )
+                resultText.text = getString(R.string.resultado_texto, dogAge)
+            }
+
+
+        }
     }
+
+
 }
