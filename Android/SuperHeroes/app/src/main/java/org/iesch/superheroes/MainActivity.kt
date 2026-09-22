df22c12e6aa76bbcbdb2d49f890d6b0aad6b47f8
arodriguezgim <arodriguezg@iesch.org>
2026-09-22 19:30:37 +0200

    Fin 1 clase

--- Android/SuperHeroes/app/src/main/java/org/iesch/superheroes/MainActivity.kt
+++ Android/SuperHeroes/app/src/main/java/org/iesch/superheroes/MainActivity.kt
@@ -1,6 +1,8 @@
 package org.iesch.superheroes
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // A partir de aqui introduzo el codigo necesario
+        val botonGuardar = findViewById<Button>(R.id.btn_guardar)
+
+        botonGuardar.setOnClickListener {
+            // Qué quiero hacer cuando pulso el Boton Guardar
+            irADetailActivity()
+        }
+
+
+
+
     }
+
+    fun irADetailActivity() {
+        // Creamos el objeto Intent
+        val intent = Intent(this, DetailActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
