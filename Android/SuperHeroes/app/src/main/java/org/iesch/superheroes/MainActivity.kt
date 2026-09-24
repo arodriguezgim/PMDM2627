2458d46bea0a80879003f33675a5f286bd2f59e9
arodriguezgim <arodriguezg@iesch.org>
2026-09-24 18:10:02 +0200

    Mostramos la foto

--- Android/SuperHeroes/app/src/main/java/org/iesch/superheroes/MainActivity.kt
+++ Android/SuperHeroes/app/src/main/java/org/iesch/superheroes/MainActivity.kt
@@ -1,12 +1,16 @@
 package org.iesch.superheroes
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.health.connect.datatypes.units.Power
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.RatingBar
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -15,39 +19,54 @@ import org.iesch.superheroes.model.SuperHeroe
 
 class MainActivity : AppCompatActivity() {
 
-    // 1 - Creamos la variable lateinit porque la vamos a inicializar luego
+
     private lateinit var binding: ActivityMainBinding
+    // 1 - Creamos una variable que va a manejar el resultado de haber hecho la foto
+    private lateinit var  heroImage: ImageView
+    private var heroBitmap: Bitmap? = null
+    private val getContent = registerForActivityResult(ActivityResultContracts.TakePicturePreview() ){
+        //Esto nos va a devolver un  objeto de tipo bitmap
+        bitmap ->
+            heroBitmap = bitmap
+            heroImage.setImageBitmap(heroBitmap)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        // 2 - Inicializamos el binding
         binding = ActivityMainBinding.inflate(layoutInflater)
-        // 3 - Usamos el binding para inflar la vista
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        // A partir de aqui introduzo el codigo necesario
-        // val botonGuardar = findViewById<Button>(R.id.btn_guardar)
+
+        // 2
+        heroImage = binding.heroImage
+        binding.heroImage.setOnClickListener {
+            abrirCamara()
+        }
+
+
         binding.btnGuardar.setOnClickListener {
-            // Obtenemos los valores al momento de hacer click
             val superHeroName = binding.heroNameEdit.text.toString()
             val alterEgo = binding.alterEgoEdit.text.toString()
             val bio = binding.alterEgoEdit.text.toString()
             val power = binding.power.rating
-            // 2 - Me creo el objeto SuperHeroe
             val superHeroe = SuperHeroe(superHeroName,alterEgo,bio,power)
-
-            // Qué quiero hacer cuando pulso el Boton Guardar
             irADetailActivity(superHeroe)
         }
 
 
 
 
+    }
+
+    fun abrirCamara() {
+        // 3 - Abrimos la camara llamando al getcontent launch
+    getContent.launch(null)
     }
 
     fun irADetailActivity(superHeroe: SuperHeroe) {
